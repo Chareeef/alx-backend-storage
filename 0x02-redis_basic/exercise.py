763c5c5585fc92a45167eef1b6f01670c9d35e10
Youssef Charif Hamidi <youssef.charif.h@gmail.com>
2024-03-27 15:10:59 +0000

    Implement the get methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ Implement the Cache class """
 import redis
-from typing import Union
+from typing import Any, Callable, Union
 from uuid import uuid4
 
 
 class Cache:
     """ Encapsulate some Redis features """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """ Instantiate a Redis client and flush it """
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -27,3 +27,24 @@ class Cache:
 
         # Return the key
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Any:
+        """ Take a key string argument and return the stored value if any """
+
+        # Retrieve value
+        value = self._redis.get(key)
+
+        # Format with `fn` if relevant
+        if value and fn:
+            value = fn(value)
+
+        # Return the value
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """ Get and return the string value stored in key """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """ Get and return the integer value stored in key """
+        return self.get(key, int)
