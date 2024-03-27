3ef83dcbee993189549b4cc51ae008a2f8bd23a7
Youssef Charif Hamidi <youssef.charif.h@gmail.com>
2024-03-27 16:29:55 +0000

    Implement count_calls decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 """ Implement the Cache class """
+from functools import wraps
 import redis
 from typing import Any, Callable, Union
 from uuid import uuid4
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Decorator for store method that increments the count
+    for the __qualname__ key every time the method is called
+
+    Return the value returned by the original method
+    """
+    
+    @wraps(method)
+    def wrapper(self, data: Union[str, bytes, int, float]) -> str:
+        """ Wrapper function """
+        key = method.__qualname__
+
+        # Initialize counter to 0 if not done
+        if not self.get_int(key):
+            self.set(key, 0)
+
+        # Increment by one the number of calls
+        self.incr(key)
+
+        # Call the store method and return its returned key
+        return method(self, data)
+
+    return wrapper
 
 
 class Cache:
@@ -13,6 +39,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Generate a random key, store the input data in
@@ -48,3 +75,11 @@ class Cache:
     def get_int(self, key: str) -> Union[int, None]:
         """ Get and return the integer value stored in key """
         return self.get(key, int)
+
+    def set(self, key: str, value: Union[str, bytes, int, float]) -> None:
+        """ Set a key-value pair """
+        self._redis.set(key, value)
+
+    def incr(self, key: str) -> None:
+        """ Increment the stored value stored in that key by 1 """
+        return self._redis.incr(key)
