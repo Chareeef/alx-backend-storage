501b1895e009ad4f9f79a81fe26b9984139406ce
Youssef Charif Hamidi <youssef.charif.h@gmail.com>
2024-03-28 01:45:58 +0000

    Use wrapper

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 """ Implement get_page function """
+from functools import wraps
 import redis
 import requests
+from typing import Callable
 
 
+def count_visits(fn: Callable) -> Callable:
+    """ Decorator to track how many times a particular URL was accessed """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper function """
+
+        # Instantiate redis
+        r = redis.Redis()
+
+        # Define the count key
+        key_count = f'count:{url}'
+
+        # Get old count
+        count = r.get(key_count)
+
+        # Track number of visits to `url`
+        r.incr(key_count)
+
+        # Return fn's result
+        return fn(url)
+
+    return wrapper
+
+
+@count_visits
 def get_page(url: str) -> str:
-    """ Obtain the HTML content of a particular URL and return it
-    Also track how many times a particular URL was accessed
-    """
+    """ Obtain the HTML content of a particular URL and return it """
 
     # Instantiate redis
     r = redis.Redis()
@@ -21,15 +47,6 @@ def get_page(url: str) -> str:
         r.setex(url, 10, page)
     else:
         page = page.decode('utf-8')
-
-    # Define the count key
-    key_count = f'count:{url}'
-
-    # Get old count
-    count = r.get(key_count)
-
-    # Track number of visits to `url`
-    r.incr(key_count)
 
     # Return the page
     return page
